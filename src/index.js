b1ae869252ddb249fb85aee42654eab47931e605
Terren Peterson <terrenjpeterson@yahoo.com>
2018-01-24 21:40:03 -0500

    updated based on new images

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const makePlainText = Alexa.utils.TextUtils.makePlainText;
 const makeImage     = Alexa.utils.ImageUtils.makeImage;
 
 // this is a public endpoint for the background image
-const backgroundImage = "https://s3.amazonaws.com/camerongallagherfoundation/image.jpg";
+const backgroundImage = "https://s3.amazonaws.com/camerongallagherfoundation/images/background.png";
 
 // this is the error message given if someone attempts to play a video without an Alexa
 const nonVideoMessage = "Sorry, this plays a video which requires an Echo Show or Spot.";
@@ -55,8 +55,8 @@ const quotes = [
 
 // this is the array of individual mindful moments files stored in S3
 const mindfulMoments = [
-    "Alexa_Reed_MindfulMoment_Breathing.mp3",
-    "Alexa_Reed_Mindfulmoment_Smile.mp3"
+    { "audio":"Alexa_Reed_MindfulMoment_Breathing.mp3", "video":"mindfullness_video_breathing.mp4" },
+    { "audio":"Alexa_Reed_Mindfulmoment_Smile.mp3", "video":"mindfullness_video_smile.mp4" }
 ];
 
 // this is the array of upcoming events that will be read back to the user
@@ -70,20 +70,31 @@ const APP_ID = 'amzn1.ask.skill.99b7b771-7458-4157-9a5b-76d5372e3cae';
                 
 const handlers = {
     'LaunchRequest': function () {
-        var quoteSelection = Math.floor(Math.random() * quotes.length);
+        const quoteSelection = Math.floor(Math.random() * quotes.length);
         // this is the mp3 that will be played
-        var quoteFile = quotes[quoteSelection];
+        const quoteFile = quotes[quoteSelection];
         console.log("quote selection played: " + quoteFile);
 
         // make valid SSML syntax for playing MP3
         var message = "<audio src=\"" + mediaLocation + "quotes/" + quoteFile + "\"/>";
         // add a one second break
             message = message + "<break time=\"1s\"/>";
-            message = message + "You can also ask Speak Up for a minute of mindfulness, upcoming " +
-                "events, or play Camerons song.";
+            message = message + "You can also ask Speak Up for a minute of mindfulness, " +
+                "play Camerons song, or learn about the foundation by saying 'learn more'.";
         var repeat = "If you would like to hear another quote, say read me a quote. ";
 
-        this.emit(':askWithCard', message, repeat, imageObj);
+        // check if the device has a video screen - if so - build the background template
+        if (this.event.context.System.device.supportedInterfaces.Display){
+            const builder = new Alexa.templateBuilders.BodyTemplate1Builder();
+            const template = builder.setTitle('Your Personal Coach')
+                .setBackgroundImage(makeImage(backgroundImage))
+                .setTextContent(makePlainText('Speak Up'))
+                .build();
+            this.response.speak(message).listen(repeat).renderTemplate(template);
+            this.emit(':responseReady');
+        } else {
+            this.emit(':askWithCard', message, repeat, imageObj);
+	}
     },
     'PlayCameronSong': function() {
 	// if the device is an Echo Show, play the video - else play the song
@@ -112,20 +123,34 @@ const handlers = {
             this.emit(':tellWithCard', message, imageObj);
 	}
     },
+    // this plays the mindfulness audio or video clip
     'GetMinuteMindfulness': function() {
         // generate a random number to select the mindful moments clip
-        var msgSelection = Math.floor(Math.random() * mindfulMoments.length);
+        const msgSelection = Math.floor(Math.random() * mindfulMoments.length);
 
-        // make valid SSML syntax for playing MP3
-        var message = "<audio src=\"" + mediaLocation + "mindfulMoments/" + 
-            mindfulMoments[msgSelection] + "\"/>";
-        // add some closing language to be played after the music.
-            message = message + "<break time=\"1s\"/>";
-            message = message + "You can also ask Speak Up for a quote, upcoming " +
-                "events, or play Camerons song.";            
-        var repeat = "Please say something like, read me a quote to get started. ";
+        if (this.event.context.System.device.supportedInterfaces.VideoApp) {
+            const videoObject = mindfulMoments[msgSelection].video;
+            const videoTitle = 'Mindful Moments';
+            const videoClip = mediaLocation + "videos/" + videoObject;
+            // this will be rendered when the user selects video controls
+            const metadata = {
+                'title': videoTitle
+            };
+            console.log("play video:" + videoClip);
+            this.response.playVideo(videoClip, metadata);
+            this.emit(':responseReady');
+        } else {
+            // make valid SSML syntax for playing MP3
+            var message = "<audio src=\"" + mediaLocation + "mindfulMoments/" + 
+                mindfulMoments[msgSelection].audio + "\"/>";
+            // add some closing language to be played after the music.
+                message = message + "<break time=\"1s\"/>";
+                message = message + "You can also ask Speak Up for a quote, upcoming " +
+                	"events, or play Camerons song.";            
+            var repeat = "Please say something like, read me a quote to get started. ";
 
-        this.emit(':askWithCard', message, repeat, imageObj);
+            this.emit(':askWithCard', message, repeat, imageObj);
+	}
     },
     'GetUpcomingEvents': function() {
         var message = "Here are the upcoming events. ";
@@ -147,7 +172,7 @@ const handlers = {
             "You can say, give me a quote and I will read an inspirational quote. " +
             "You can say, play cameron song and I will play a song that is a tribute to Cameron Gallagher. " +
             "You can say, give me a minute of mindfulness and I will help you relax. " +
-            "or you can say what are upcoming events, and I will list you what events the foundation has on the calendar. ";
+            "or you can say 'learn more', and I will share more details about the foundations background. ";
         var repeat = "Please say something like, read me a quote to get started. ";
         this.emit(':ask', message, repeat);
     },
@@ -177,8 +202,13 @@ const handlers = {
             this.response.playVideo(videoClip, metadata);
 	    this.emit(':responseReady');
         } else {
-            // handle error - and close the session
-            this.emit(':tell', nonVideoMessage);
+            // this is the mp3 file name for the about ckg video
+            const songFile = "audio/about_ckg_audio.mp3";
+
+            // make valid SSML syntax for playing MP3
+            const message = "<audio src=\"" + mediaLocation + songFile + "\"/>";
+
+            this.emit(':tellWithCard', message, imageObj);
         }
     },
     'Unhandled': function() {
