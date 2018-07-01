3f39cf0c5fad5386fef77992ca4448e90b9c89d7
Terren Peterson <terrenjpeterson@yahoo.com>
2018-07-01 16:11:56 -0400

    updates to enable in-skill purchasing

--- src/index.js
+++ src/index.js
@@ -43,33 +43,126 @@ const full_length_prod = 'amzn1.adg.product.f88555b5-10ae-419a-8923-b5061cb380d8
 const https = require('https');
                 
 const handlers = {
+    // this gets invoked when a new session is created. this will be driven by user requests or purchase events
+    'NewSession': function() {
+        console.log("New Session.");
+
+        if (this.event.request.type === 'LaunchRequest') {
+            this.emit('PlayMindfulMoment');
+        } else if (this.event.request.type === 'IntentRequest' && this.event.request.intent.name === 'PlayCameronSong') {
+            this.emit('PlayCameronSong');
+        // [TODO: add more intent names here to make sure if launching directly doesn't go into purchase flow]
+        } else {
+            // these are events generated from the purchase engine
+            console.log('Request received from purchase engine.');
+            this.emit('ProcessPurchaseEvent');
+        } 
+    },
     'PlayCameronSong': function() {
-	// if the device is an Echo Show, play the video - else play the song
-        if (this.event.context.System.device.supportedInterfaces.VideoApp) {
-            const videoObject = 'Cameron_song_video.mp4';
-            const videoTitle = "Cameron's Song";
-            const videoClip = mediaLocation + "videos/" + videoObject;
-            // this will be rendered when the user selects video controls
-            const metadata = {
-                'title': videoTitle
-            };
-            console.log("play video:" + videoClip);
-            this.response.playVideo(videoClip, metadata);
-            this.emit(':responseReady');
-	} else {
-            // this is the mp3 file name for Cameron's song
-            var songFile = "Cameron.mp3";
+        console.log('request made to provide information about premium version of skill');
 
-            // make valid SSML syntax for playing MP3
-            var message = "<audio src=\"" + mediaLocation + songFile + "\"/>";
-            // add a one second break
-                message = message + "<break time=\"1s\"/>";
-                message = message + "The full version of Cameron's song by Christopher Minton " +
-                    "is available on this skill, and can be purchased by saying " +
-		    "Purchase full song.";
+        var returnData = [];
 
-            this.emit(':tellWithCard', message, imageObj);
-	}
+        // Information required to invoke the API is available in the session
+        const apiEndpoint = "api.amazonalexa.com";
+        const token  = "bearer " + this.event.context.System.apiAccessToken;
+        const language    = this.event.request.locale;
+
+        // The path for the in skill products API
+        const apiPath     = "/v1/users/~current/skills/~current/inSkillProducts";
+
+        const options = {
+            host: apiEndpoint,
+            path: apiPath,
+            method: 'GET',
+            headers: {
+                "Content-Type"      : 'application/json',
+                "Accept-Language"   : language,
+                "Authorization"     : token
+            }
+        };
+
+        console.log('ISP API:' + JSON.stringify(options));
+
+        // Call the API to see if the extended version has already been purchased for this user
+        const req = https.get(options, (res) => {
+            res.setEncoding("utf8");
+
+            res.on('data', (chunk) => {
+                console.log("Chunk:" + chunk);
+                returnData += chunk;
+            });
+
+            req.on('error', (e) => {
+                console.log('Error calling InSkillProducts API: ' + e.message);
+                const message = 'Hmmm - something went wrong.  Please try again later.';
+                this.emit(':ask', message, message);
+            });
+
+            res.on('end', () => {
+                var userEntitlement = eval('(' + returnData + ')');
+                console.log("Finished. " + JSON.stringify(returnData));
+                console.log("API Code:" + res.statusCode);
+
+                // vary response based on if the user has already purchased the full version of the song
+                if (userEntitlement.inSkillProducts[0].entitled === 'ENTITLED') {
+                    if (this.event.context.System.device.supportedInterfaces.VideoApp) {
+                        const thanksMessage = "Thank you for purchasing the full version of this skill. " +
+                            "You may play the entire song as you choose.";
+                            "Please say, Play Cameron Song to begin.";
+                        const thanksReprompt = "If you would like to begin, please say Play Cameron Song.";
+
+                        this.emit(':ask', thanksMessage, thanksReprompt);
+		    } else {
+			console.log("Playing Full Version of Song");
+                        // make valid SSML syntax for playing MP3
+                        var message = "<audio src=\"" + mediaLocation + "audio/CameronsSongFull-Part1.mp3" + "\"/>";
+			//	      "<audio src=\"" + mediaLocation + "audio/CameronsSongFull-Part2.mp3" + "\"/>";
+			//	      "<audio src=\"" + mediaLocation + "audio/CameronsSongFull-Part3.mp3" + "\"/>" +
+			//	      "<audio src=\"" + mediaLocation + "audio/CameronsSongFull-Part4.mp3" + "\"/>";
+                        // add a one second break
+                            message = message + "<break time=\"1s\"/>";
+                            message = message + "Thanks for purchasing Cameron's song by Christopher Minton. ";
+
+			console.log("Message:" + message);
+
+                        this.emit(':tellWithCard', message, imageObj);
+		    }
+                } else {
+		    // this is the flow if the full song has not been purchased
+        	    // if the device is an Echo Show, play the video - else play the song
+        	    if (this.event.context.System.device.supportedInterfaces.VideoApp) {
+            		const videoObject = 'Cameron_song_video.mp4';
+            		const videoTitle = "Cameron's Song";
+            		const videoClip = mediaLocation + "videos/" + videoObject;
+            		// this will be rendered when the user selects video controls
+            		const metadata = {
+                	    'title': videoTitle
+            		};
+            		console.log("play video:" + videoClip);
+            		this.response.playVideo(videoClip, metadata);
+            		this.emit(':responseReady');
+        	    } else {
+			console.log("Playing Short Version of Song");
+            		// this is the mp3 file name for Cameron's song
+            		var songFile = "Cameron.mp3";
+
+            		// make valid SSML syntax for playing MP3
+            		var message = "<audio src=\"" + mediaLocation + songFile + "\"/>";
+            		// add a one second break
+                	    message = message + "<break time=\"1s\"/>";
+                	    message = message + "The full version of Cameron's song by Christopher Minton " +
+                    		"is available on this skill, and can be purchased by saying " +
+                    		"Purchase full song.";
+            		const repeat = "If you would like to purchase the full version of the song, " +
+                	    "please say, purchase full song.";
+
+			console.log("Message:" + message);
+            		this.emit(':askWithCard', message, repeat, imageObj);
+        	    }
+                }
+            });
+        });
     },
     // this plays the mindfulness audio or video clip
     'GetMinuteMindfulness': function() {
@@ -315,55 +408,81 @@ const handlers = {
         this.emit(':responseReady');
         console.log(JSON.stringify(this.response));
     },
-    // this gets invoked after a purchase request
-    'NewSession': function() {
-	console.log("New Session.");
-	
-	if (this.event.request.type === 'LaunchRequest') {
+    // this function will play a random audio or video clip
+    'PlayMindfulMoment': function() {
+        console.log("Playing Mindful Moment");
+        var message = "Here is today's moment of mindfullness exercise.";
 
-            var message = "Here is today's moment of mindfullness exercise.";
-            console.log("Launch Request Enabled");
+        // generate a random number to select the mindful moments clip
+        const msgSelection = Math.floor(Math.random() * mindfulMoments.length);
 
-            // generate a random number to select the mindful moments clip
-            const msgSelection = Math.floor(Math.random() * mindfulMoments.length);
+        // check if the device is an echo show or something with a screen
+        if (this.event.context.System.device.supportedInterfaces.VideoApp) {
+            console.log("Playing Mindful Video " + msgSelection);
 
-            // check if the device is an echo show or something with a screen
-            if (this.event.context.System.device.supportedInterfaces.VideoApp) {
-            	console.log("Playing Mindful Video " + msgSelection);
+            const videoObject = mindfulMoments[msgSelection].video;
+            const videoTitle  = 'Mindful Moments';
+            const videoClip   = mediaLocation + "videos/" + videoObject;
 
-            	const videoObject = mindfulMoments[msgSelection].video;
-            	const videoTitle  = 'Mindful Moments';
-            	const videoClip   = mediaLocation + "videos/" + videoObject;
+            // this will be rendered when the user selects video controls
+            const metadata = {
+                'title': videoTitle
+            };
 
-            	// this will be rendered when the user selects video controls
-            	const metadata = {
-                    'title': videoTitle
-            	};
-
-            	this.response.playVideo(videoClip, metadata);
-            	this.emit(':responseReady');
-            } else {
-            	console.log("Playing Mindful Audio " + msgSelection);
-            	// make valid SSML syntax for playing MP3
-            	var message = "Here is today's moment of mindfullness exercise.";
-                    message = message + "<audio src=\"" + mediaLocation + "mindfulMoments/" +
-                    mindfulMoments[msgSelection].audio + "\"/>";
-            	// add some closing language to be played after the music.
-                    message = message + "<break time=\"1s\"/>";
-                    message = message + "You can also ask Speak Up for a quote or play Camerons song.";
-                var repeat = "Please say something like, read me a quote to get started. ";
-
-            	this.emit(':askWithCard', message, repeat, imageObj);
-            }
+            this.response.playVideo(videoClip, metadata);
+            this.emit(':responseReady');
         } else {
-	    console.log('Request received from purchase engine.');
+            console.log("Playing Mindful Audio " + msgSelection);
+            // make valid SSML syntax for playing MP3
+            var message = "Here is today's moment of mindfullness exercise.";
+                message = message + "<audio src=\"" + mediaLocation + "mindfulMoments/" +
+                mindfulMoments[msgSelection].audio + "\"/>";
+            // add some closing language to be played after the music.
+                message = message + "<break time=\"1s\"/>";
+                message = message + "You can also ask Speak Up for a quote or play Camerons song.";
+            const repeat = "Please say something like, read me a quote to get started. ";
 
-	    const message = "Thanks for purchasing the skill. If you would like to play the full length version " +
-	        "now, please say Play Cameron Song.";
-	    const reprompt = "If you would like to play the song, please say Play Cameron Song.";
+            console.log("Message Played:" + message);
 
-	    this.emit(':ask', message, reprompt);
-	}
+            this.emit(':askWithCard', message, repeat, imageObj);
+        }
+    },
+    // this processes the events generated after a purchase transaction flow is completed
+    'ProcessPurchaseEvent': function() {
+        console.log("Purchase Result: " + this.event.request.payload.purchaseResult);
+
+        if (this.event.request.payload.purchaseResult === 'ACCEPTED') {
+            if (this.event.request.name === 'Cancel') {
+                console.log("Successfully requested a refund.");
+                var refundMessage = "Your request for a refund has been successfully processed. " +
+                    "Please follow the instructions on the Alexa Companion App for more details.";
+                this.response.speak(refundMessage);
+                this.emit(':responseReady');
+            } else {
+                console.log("Received acceptance message from purchase.");
+                const message = "Thanks for purchasing. If you would like to play the full length version " +
+                    "now, please say Play Cameron Song.";
+                const reprompt = "If you would like to play the song, please say Play Cameron Song.";
+
+                this.emit(':ask', message, reprompt);
+            }
+        } else if (this.event.request.payload.purchaseResult === 'ALREADY_PURCHASED') {
+            console.log("Attempt to purchase song that already has been made.");
+            this.response.speak("You have already purchased the extended version of the song.");
+            this.emit(':responseReady');
+        } else if (this.event.request.payload.purchaseResult === 'DECLINED') {
+            console.log("Customer began purhase transaction, then declined.");
+            this.response.speak("Let's go ahead and try something else.");
+            this.emit(':responseReady');
+        } else if (this.event.request.payload.purchaseResult === 'ERROR') {
+            console.log("Error received during purchase transaction.");
+            this.response.speak("Hmmm. Something didn't work. Please try again later.");
+            this.emit(':responseReady');
+        } else {
+            console.log("Unknown error occurred during new session creation.");
+            this.response.speak('Sorry, an error occurred. Please try again');
+            this.emit(':responseReady');
+        }
     },
     'Unhandled': function() {
         console.log("UNHANDLED");
